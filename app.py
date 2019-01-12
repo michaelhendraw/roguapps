382594fb3b47a694336ee0e8160920d003268bb9
Michael Hendra Wijaya <michaelhendraw@gmail.com>
2019-01-12 16:04:19 +0700

    mapel 3

--- app.py
+++ app.py
@@ -82,11 +82,12 @@ def handle_text_message(event):
     if session_bytes is not None:
         session = json.loads(session_bytes.decode("utf-8"))
 
-    postbacks = event.postback.data.split('&')
     postback = []
-    for p in postbacks:
-        ps = p.split('=')
-        postback[ps[0]] = ps[1]
+    if event.postback is not None:
+        postbacks = event.postback.data.split('&')
+        for p in postbacks:
+            ps = p.split('=')
+            postback[ps[0]] = ps[1]
 
     print('\n\nHERE, request event:', event)
     print("\n\nHERE, session:", session)
